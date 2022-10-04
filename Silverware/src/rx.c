3ae294fb4d4ebf90dac5776a67e7efeae936eca3
NotFastEnuf <schrock7903@gmail.com>
2022-10-04 15:53:27 -0400

    finished autocentering, rates, and expo in apply_rates() function

--- Silverware/src/rx.c
+++ Silverware/src/rx.c
@@ -16,7 +16,7 @@ float autocenter[3];
 float rates[3];
 
 //probably need to make the returns of this function conditional on flight mode to support mixed combos from horizon and racemode and to eliminate the need for an acro function
-float get_angle_expo (int axis){
+float get_flitemode_expo (int axis){
 	if (axis==0) return ANGLE_EXPO_ROLL;
 	if (axis==2) return ANGLE_EXPO_PITCH;
 	if (axis==3) return ANGLE_EXPO_YAW;
@@ -40,39 +40,33 @@ void apply_rates(){
 	//get raw rx[] from radio
 		rxcopy[i] = rx[i];
 	//autocentering algorithm detects trimmed centerpoint	
-		//static float autocenter[3];
 		static float lastrx[3];
 		static unsigned int consecutive[3];
-		rxcentered[i] = (rxcopy[i] - autocenter[i]);
-		if ( rxcopy[i] == lastrx[i] ){
+		rxcentered[i] = (rx[i] - autocenter[i]);
+		if ( rx[i] == lastrx[i] ){
 			consecutive[i]++;
 		}else{
 			consecutive[i] = 0;
 		}
-		lastrx[i] = rxcopy[i];
-		if ( consecutive[i] > 750 && fabsf( rxcopy[i]) < 0.1f ){
-			autocenter[i] = rxcopy[i];
+		lastrx[i] = rx[i];
+		if ( consecutive[i] > 750 && fabsf( rx[i]) < 0.1f ){
+			autocenter[i] = rx[i];
 		}
-	//if in a leveled mode - any leveled axis will be using rxcentered[] as stick input so we can aply ANGLE_EXPO_AXIS to rxcentered[] unconditionally
-	//apply low rates to rxcentered (used in levelmode) - sticks are already corrected to a 0 centerpoint so we can use the low rates multiplier with multiplication
+	//apply high/low rates to rxcentered
 		rxcentered[i] = rxcentered[i] * rate_multiplier;
-	//finally we apply expo normally but also cut expo by a factor of low rates multiplier when low rates are active
-		if (get_angle_expo(i) > 0.01f) rxcentered[i] = rcexpo(rx[i], (rate_multiplier * get_angle_expo(i)) ); 	
+	//apply expo normally but also cut expo by a factor of low rates multiplier when low rates are active
+		if (get_flitemode_expo(i) > 0.01f) rxcentered[i] = rcexpo(rxcentered[i], (rate_multiplier * get_flitemode_expo(i)) );
+	//rxcentered[] is now adjusted back to 0 centering even if pilot has trimmed the radio and is ready to be used by the pid controller for leveled modes
+	//the autocenter[] amount is the amount that each axis has been trimmed and this value will be forwarded to the mixer as a centerpoint for any leveled axis
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++		
-	//if in sport or manual mode
-	//first we nned to scale and remap sticks since pilot trims will shift the detected stick range beyond 1 or -1
+	//for sport and manual mode the rxcentered[] sticks need to be shifted back to restore pilot trims as rxcopy[i] is passed directly through the mixer
+		rxcopy[i] = (rxcentered[i] + autocenter[i]);		
+	//but we need to scale and remap sticks since adding back the pilot trims will have shifted the detected stick range beyond 1 or -1
 	//the result should accept incoming sticks that are out of range due to trims and will stretch / squeeze throws to fit back into range at the expense of resolution
-	//but we also need to apply the low rates if selected.  But using low rates multiplier as a factor against rxcopy[] will cause a loss of trim on the control surfaces when selecting low rates.
-	//So low rates can also be performed as part of the map function substituting 1.0 in the previous stick scale for low rates multiplier - this will retain the trimmed pilot centerpoint.
-	
-		//scale raw sticks ....  range: from [(detected trimmed centerpoint) to (1.0 +/- detected trim amount)] to [(-1 to detected trimmed center) and (detected trim center to +1)]
-		//if (rxcopy[i] > autocenter[i])  mapf the things that need mapping and scaling
-
-			
-	
-	//finally apply expo from detected trim center to endpoints
-		//this one will take some new math
-	//limit both rxcopy[] and rxcentered[]
+	//but also need to compensate for low rates if selected just in case (this may be redundant)	
+		if (rxcopy[i] > autocenter[i]) rxcopy[i] = mapf( rxcopy[ i ], autocenter[i], rate_multiplier + autocenter[i], autocenter[i], rate_multiplier );
+		if (rxcopy[i] < autocenter[i]) rxcopy[i] = mapf( rxcopy[ i ], autocenter[i], -rate_multiplier + autocenter[i], autocenter[i], -rate_multiplier );		
+	//limit both rxcopy[] and rxcentered[] just in case (should have been done by the rcexpo function)
 		limitf(&rxcopy[i], 1.0);
 		limitf(&rxcentered[i], 1.0);
 	//end for loop
